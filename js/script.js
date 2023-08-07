372223189a7c0774839bf2de121d74b032d2255d
sajjadsunnyofficial <sajjadhossainsunny.official@gmail.com>
2023-08-07 10:29:16 +0600

    counter js separated

--- js/script.js
+++ js/script.js
@@ -34,26 +34,3 @@ button.addEventListener("click", function () {
     result.style.color = "red";
   }
 });
-
-
-
-let counter = document.querySelectorAll(".count");
-let arr = Array.from(counter);
-
-arr.map((items) => {
-  let count = 0;
-
-  function counterUp() {
-    count++
-    items.innerHTML = count;
-
-    if (count == items.dataset.change) {
-      clearInterval(stop);
-    }
-  }
-
-  let stop = setInterval(function () {
-    counterUp();
-  }, 1000 / items.dataset.change);
-});
-
